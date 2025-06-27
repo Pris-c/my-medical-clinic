ee6799b95d6e4ff10719799a2fecca942062d929
Priscila C <priscilac@email.com>
2025-06-27 15:13:11 +0100

    initial setup

---
+++ src/main/java/com/prisc/my_medical_clinic/MyMedicalClinicApplication.java
@@ -0,0 +1,13 @@
+package com.prisc.my_medical_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMedicalClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMedicalClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prisc/my_medical_clinic/MyMedicalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prisc.my_medical_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMedicalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
