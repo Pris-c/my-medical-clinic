78fede6f16e04a34e115d61f088c20fcbf34094c
Priscila C <priscilac@email.com>
2025-06-27 19:25:48 +0100

    update UserRepository: Add methods existsUserByEmail, and existsUsersByNif

--- src/main/java/com/prisc/my_medical_clinic/repository/UserRepository.java
+++ src/main/java/com/prisc/my_medical_clinic/repository/UserRepository.java
@@ -9,4 +9,8 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     User findByEmail(String email);
+
+    boolean existsUserByEmail(String email);
+
+    boolean existsUsersByNif(String nif);
 }
