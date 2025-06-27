454a46d06637291d894757cbe9209e8c299a4c1b
Priscila C <priscilac@email.com>
2025-06-27 15:40:04 +0100

    create UserRepository containing findByEmail method

---
+++ src/main/java/com/prisc/my_medical_clinic/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.prisc.my_medical_clinic.repository;
+
+import com.prisc.my_medical_clinic.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByemail(String email);
+}
