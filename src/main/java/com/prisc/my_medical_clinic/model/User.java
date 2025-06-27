dbc632b133766741b3995c04888079569ba27634
Priscila C <priscilac@email.com>
2025-06-27 15:34:48 +0100

    create Entity User (id, name, email, password, nif)

---
+++ src/main/java/com/prisc/my_medical_clinic/model/User.java
@@ -0,0 +1,42 @@
+package com.prisc.my_medical_clinic.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private UUID id;
+
+    @NotBlank
+    @Size(min = 1, max = 200)
+    private String name;
+
+    @Email
+    @Column(unique = true)
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{9,}$",
+            message = "A senha deve ter pelo menos 9 caracteres, incluindo letra maiúscula, minúscula, número e caractere especial"
+    )
+    private String password;
+
+    @Column(unique = true, length = 9)
+    @Size(min = 9, max = 9, message = "NIF inválido")
+    @Pattern(regexp = "\\d{9}", message = "O NIF deve conter apenas números")
+    private String nif;
+
+}
