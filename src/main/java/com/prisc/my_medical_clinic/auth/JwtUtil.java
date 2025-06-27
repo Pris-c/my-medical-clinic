06e52b098dc27190bb03e1fbc96bddc6f0065a43
Priscila C <priscilac@email.com>
2025-06-27 18:10:24 +0100

    implement JwtUtil

---
+++ src/main/java/com/prisc/my_medical_clinic/auth/JwtUtil.java
@@ -0,0 +1,98 @@
+package com.prisc.my_medical_clinic.auth;
+
+import com.prisc.my_medical_clinic.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwtKey}")
+    private String SECRET;
+    private final long ACCESS_TOKEN_VALIDITY_SECONDS = 3600;
+    private final String TOKEN_HEADER = "Authorization";
+    private final String TOKEN_PREFIX = "Bearer ";
+
+    private JwtParser jwtParser;
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+        this.jwtParser = Jwts.parser().verifyWith(key).build();
+    }
+
+    public String createToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("name", user.getName());
+
+        Date tokenCreateTime = new Date();
+        Date tokenExpiration = new Date(tokenCreateTime.getTime() + TimeUnit.MINUTES.toMillis(ACCESS_TOKEN_VALIDITY_SECONDS));
+
+           return Jwts.builder()
+                .claims(claims)
+                .subject(user.getEmail())
+                .issuedAt(tokenCreateTime)
+                .expiration(tokenExpiration)
+                .signWith(key, Jwts.SIG.HS256)
+                .compact();
+    }
+
+    private Claims parseJwtClaims(String token) {
+        return jwtParser.parseSignedClaims(token).getPayload();
+    }
+
+    public Claims resolveClaims(HttpServletRequest req) {
+        try {
+            String token = resolveToken(req);
+            if (token != null) {
+                return parseJwtClaims(token);
+            }
+            return null;
+        } catch (ExpiredJwtException ex) {
+            req.setAttribute("expired", ex.getMessage());
+            throw ex;
+        } catch (Exception ex) {
+            req.setAttribute("invalid", ex.getMessage());
+            throw ex;
+        }
+    }
+
+
+    public String resolveToken(HttpServletRequest request) {
+
+        String bearerToken = request.getHeader(TOKEN_HEADER);
+        if (bearerToken != null && bearerToken.startsWith(TOKEN_PREFIX)) {
+            return bearerToken.substring(TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+
+    public boolean validateClaims(Claims claims) {
+        return claims.getExpiration().after(new Date());
+    }
+
+    public String getEmail(Claims claims) {
+        return claims.getSubject();
+    }
+
+    private String getRole(Claims claims) {
+        Object raw = claims.get("roles");
+        return raw instanceof String ? (String) raw : null;
+    }
+
+}
